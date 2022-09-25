d2040b596205e0be2c4d2f4f5ee2e492f9fbff6b
zippywallet <113642913+zippywallet@users.noreply.github.com>
2022-09-25 13:03:30 +0000

    Add files via upload

---
+++ v1/include/user_dash.php
@@ -0,0 +1,36 @@
+<h3 class="mtitle" > Your account status is <span class="tag_success">Approved</span></h3>
+        
+                <p class="uk-text-lead uk-text-muted">Congratulations!! your account account has been approved by our admins.</p>
+    
+              
+<!-- card one  -->
+<div class="uk-card uk-card-category hyper_mt uk-card-default uk-card-hover uk-card-body uk-inline uk-border-rounded uk-width-1-1">
+                <a class="uk-position-cover" href="./skchecker"></a>
+               
+                <div class="uk-article-meta uk-flex uk-flex-middle">
+                 
+                <div class="uk-border-circle uk-avatar-small s_logo" ></div>
+                  <div>
+                   <h3><a href=""> Sk Based Checker</a></h3>
+                   <span class="tag_active">Online</span> 
+                   
+                  </div>
+                </div>
+              </div>
+              <!-- card two  -->
+              <div class="uk-card uk-card-category hyper_mt uk-card-default uk-card-hover uk-card-body uk-inline uk-border-rounded uk-width-1-1">
+                <a class="uk-position-cover" href="./checker"></a>
+               
+                <div class="uk-article-meta uk-flex uk-flex-middle">
+                  <div class="uk-border-circle uk-avatar-small s_logo" ></div>
+                  <div>
+                   <h3><a href=""> Non Sk Checker</a></h3>
+                   <span class="tag_active">Online</span>
+                    
+                  </div>
+                </div>
+              </div>
+              
+              <center><p class="uk-text-lead uk-text-muted">Here is your account token [keep it in safe place]</p></center>
+                <input type="text" class="uk-form-controls access_input" value="<?php echo $access_tok; ?>" readonly>
+              
